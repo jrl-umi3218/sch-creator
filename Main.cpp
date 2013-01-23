a720266d36ebc57bcea0a555ac4c4d6752de0939
Joris Vaillant <joris.vaillant@gmail.com>
2013-01-23 16:17:53 +0100

    Reindent source.

--- Main.cpp
+++ Main.cpp
@@ -10,59 +10,59 @@ namespace po = boost::program_options;
 
 int main(int argc, char **argv)
 {
-        double r, R;
+  double r, R;
 
-        po::options_description desc("Allowed options");
-        desc.add_options()
-                ("help,h", "produce help message")
-                ("r,r", po::value<double>(&r)->default_value(.2), "small sphere radius")
-                ("R,R", po::value<double>(&R)->default_value(300.), "big sphere radius")
-                ("input-file", po::value<string>(), "input file")
-                ("output-file", po::value<string>(), "output file")
-                ("poly", po::value<bool>()->default_value(false), "generate the polyhedron of the STP-BV");
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,h", "produce help message")
+    ("r,r", po::value<double>(&r)->default_value(.2), "small sphere radius")
+    ("R,R", po::value<double>(&R)->default_value(300.), "big sphere radius")
+    ("input-file", po::value<string>(), "input file")
+    ("output-file", po::value<string>(), "output file")
+    ("poly", po::value<bool>()->default_value(false), "generate the polyhedron of the STP-BV");
 
-        po::positional_options_description pos;
-        pos.add("input-file", 1);
-        pos.add("output-file", 1);
+  po::positional_options_description pos;
+  pos.add("input-file", 1);
+  pos.add("output-file", 1);
 
-        po::variables_map vm;
-        po::store(po::command_line_parser(argc, argv).options(desc).positional(pos).run(), vm);
-        po::notify(vm);
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(pos).run(), vm);
+  po::notify(vm);
 
-        if (vm.count("help")) {
-            cout << desc << endl;
-            return 1;
-        }
+  if (vm.count("help")) {
+    cout << desc << endl;
+    return 1;
+  }
 
-        cout << "\n STP-BV parameters: r = " << r << ", R = " << R << std::endl << std::endl;
+  cout << "\n STP-BV parameters: r = " << r << ", R = " << R << std::endl << std::endl;
 
-        if (vm.count("input-file") && vm.count("output-file"))
-        {
-                SCD::SmoothHullGeneratorVVR sg(r, R);
-                fstream testfile;
-                string input = vm["input-file"].as<string>();
-                string output = vm["output-file"].as<string>();
+  if (vm.count("input-file") && vm.count("output-file"))
+  {
+    SCD::SmoothHullGeneratorVVR sg(r, R);
+    fstream testfile;
+    string input = vm["input-file"].as<string>();
+    string output = vm["output-file"].as<string>();
 
-                testfile.open(input.c_str());
+    testfile.open(input.c_str());
 
-                if (testfile.is_open())
-                {
-                        cout << "Opening "<< input << endl;
+    if (testfile.is_open())
+    {
+      cout << "Opening "<< input << endl;
 
-                        testfile.close();
+      testfile.close();
 
-                        sg.loadGeometry(input);
-                        if(vm["poly"].as<bool>())
-                          sg.computeVVR_WithPolyhedron(output);
-                        else
-                          sg.computeVVR_Prime(output);
+      sg.loadGeometry(input);
+      if(vm["poly"].as<bool>())
+        sg.computeVVR_WithPolyhedron(output);
+      else
+        sg.computeVVR_Prime(output);
 
-                        cout << "STP-BV Created, output file "<< output << endl;
-                        cout << "Successfully finished" << endl;
-                }
-                else
-                        cout << "Failed to open " << input << endl;
-        }
+      cout << "STP-BV Created, output file "<< output << endl;
+      cout << "Successfully finished" << endl;
+    }
+    else
+      cout << "Failed to open " << input << endl;
+  }
 
-        return 0;
+  return 0;
 }
--- SmoothHullGeneratorVVR.h
+++ SmoothHullGeneratorVVR.h
@@ -1,12 +1,12 @@
 /*! \file SmoothHullGeneratorVVR.h
-*  \brief Declaration file of the Class SmoothHullGeneratorVVR
-*  \author Escande Adrien
-*  \author Cochet-Grasset Amelie
-*  \author Benallegue Mehdi
-*  \author Sylvain Miossec
-*  \version 0.0.0
-*	\date 07.10.23
-*/
+ *  \brief Declaration file of the Class SmoothHullGeneratorVVR
+ *  \author Escande Adrien
+ *  \author Cochet-Grasset Amelie
+ *  \author Benallegue Mehdi
+ *  \author Sylvain Miossec
+ *  \version 0.0.0
+ *	\date 07.10.23
+ */
 
 #pragma once
 
@@ -25,201 +25,201 @@
 
 
 /*! \namespace SCD
-*  \brief Smooth Collision Detection Namespace
-*/
+ *  \brief Smooth Collision Detection Namespace
+ */
 
 namespace SCD
 {
-	typedef vector3<double> vector3d;
+  typedef vector3<double> vector3d;
 
-	/*! \class VVRSFace
-	*	\brief %Class VVRSFace
-	*	\author Cochet-Grasset Amelie
-	*	\version 0.0.0
-	*	\date 07.10.23
-	*	\bug None
-	*	\warning None
-	*  
-	* A triangular face along with the center of its related sphere
-	*/
-	struct VVRSFace
-	{
-		int			_point1;
-		int			_point2;
-		int			_point3;
-		vector3d	_center;
-	};
+  /*! \class VVRSFace
+   *	\brief %Class VVRSFace
+   *	\author Cochet-Grasset Amelie
+   *	\version 0.0.0
+   *	\date 07.10.23
+   *	\bug None
+   *	\warning None
+   *  
+   * A triangular face along with the center of its related sphere
+   */
+  struct VVRSFace
+  {
+    int			_point1;
+    int			_point2;
+    int			_point3;
+    vector3d	_center;
+  };
 
-	/*! \class VVRsphere
-	*	\brief %Class VVRsphere
-	*	\author Cochet-Grasset Amelie
-	*	\version 0.0.0
-	*	\date 07.10.22
-	*	\bug None
-	*	\warning None
-	*  
-	* A sphere
-	*/
-	struct VVRsphere
-	{
-		double		_radius;
-		vector3d		_center;
-	};
+  /*! \class VVRsphere
+   *	\brief %Class VVRsphere
+   *	\author Cochet-Grasset Amelie
+   *	\version 0.0.0
+   *	\date 07.10.22
+   *	\bug None
+   *	\warning None
+   *  
+   * A sphere
+   */
+  struct VVRsphere
+  {
+    double		_radius;
+    vector3d		_center;
+  };
 
-	/*! \class VVRtorus
-	*	\brief %Class VVRtorus
-	*	\author Cochet-Grasset Amelie
-	*	\version 0.0.0
-	*	\date 07.10.22
-	*	\bug None
-	*	\warning None
-	*  
-	* A Torus without the inner radius
-	*/
-	struct VVRtorus
-	{
-		double		_extRadius;
-		vector3d	_center;
-		vector3d		_normal;
-	};
+  /*! \class VVRtorus
+   *	\brief %Class VVRtorus
+   *	\author Cochet-Grasset Amelie
+   *	\version 0.0.0
+   *	\date 07.10.22
+   *	\bug None
+   *	\warning None
+   *  
+   * A Torus without the inner radius
+   */
+  struct VVRtorus
+  {
+    double		_extRadius;
+    vector3d	_center;
+    vector3d		_normal;
+  };
 
-	/*! \class Cone
-	*	\brief %Class Cone
-	*	\author Cochet-Grasset Amelie
-	*	\version 0.0.0
-	*	\date 07.07
-	*	\bug None
-	*	\warning None
-	*  
-	* A sphere
-	*/
-	struct Cone
-	{
-		double _cosangle;
-		vector3d		 _axis;
-	};
+  /*! \class Cone
+   *	\brief %Class Cone
+   *	\author Cochet-Grasset Amelie
+   *	\version 0.0.0
+   *	\date 07.07
+   *	\bug None
+   *	\warning None
+   *  
+   * A sphere
+   */
+  struct Cone
+  {
+    double _cosangle;
+    vector3d		 _axis;
+  };
 
-	typedef std::pair<int, vector3d> VVRplane;
-	typedef std::pair<int, Cone> VVRcone;
+  typedef std::pair<int, vector3d> VVRplane;
+  typedef std::pair<int, Cone> VVRcone;
 
-	struct faceVVR
-	{
-		VVRplane _plane1;
-		VVRplane _plane2;
-		VVRplane _plane3;
-	};
-
-
-	/*! \class SmoothHullGeneratorVVR
-	*	\brief %Class SmoothHullGeneratorVVR
-	*	\author Escande Adrien
-	*   \author Cochet-Grasset Amelie
-	*	\version 0.0.0
-	*	\date 07.10.23
-	*	\bug None
-	*	\warning None
-	*  
-	* This class read a 3d points cloud and compute its smooth convex hull made of spheres and tori
-	*/
-	
+  struct faceVVR
+  {
+    VVRplane _plane1;
+    VVRplane _plane2;
+    VVRplane _plane3;
+  };
 
 
-	class SmoothHullGeneratorVVR
-	{
-	#pragma region "public structures"
-	public:
-	#pragma endregion
-	#pragma region "protected structures"
-	protected:
-	#pragma endregion
-	#pragma region "private structures"
-	private:
-		struct turnData
-		{
-			int			_p1;				//we turn around [_p1,_p2]
-			int			_p2;
-			int			_p3;				//we remember the third point of the previous sphere
-			vector3d		_previousCenter;	//center of the sphere we turn (/come) from
-		};
-	#pragma endregion
-	#pragma region "public data"
-	public:
-	#pragma endregion
-	#pragma region "constructors"
-	private:
-	protected:
-	public:
-		SmoothHullGeneratorVVR(double r, double R);
-		SmoothHullGeneratorVVR(std::vector<vector3d>& points, double r, double R);
-	#pragma endregion
-	#pragma region "accessors"
-	public:
-	#pragma endregion
-	#pragma region "public methods"
-	public:
-		//WARNING : be sure that there is no double points in the cloud
-		void	loadGeometry(const std::string& filename);
-		void	compute3DSMaxHull(const std::string& rootPath);
-		void	computeSmoothHull(std::vector<vector3d>& outPoints, std::vector<VVRSFace>& outSFaces);
-		void	computeVVR(const std::string& filename);
-		void	computeVVR_WithPolyhedron(const std::string& filename);
-		void	computeVVR_Prime(const std::string& filename);
+  /*! \class SmoothHullGeneratorVVR
+   *	\brief %Class SmoothHullGeneratorVVR
+   *	\author Escande Adrien
+   *   \author Cochet-Grasset Amelie
+   *	\version 0.0.0
+   *	\date 07.10.23
+   *	\bug None
+   *	\warning None
+   *  
+   * This class read a 3d points cloud and compute its smooth convex hull made of spheres and tori
+   */
 
-	#pragma endregion
-	#pragma region "public static methods"
-	public:
-	#pragma endregion
-	#pragma region "protected methods"
-	protected:
-	#pragma endregion
-	#pragma region "protected static methods"
-	private:
-	#pragma endregion
-	#pragma region "private methods"
-	private:
-		bool	findCenter(int p1, int p2, int p3, vector3d &center);
-		bool	isInSphere(vector3d &point, vector3d &center);
-		double	distInSphere(vector3d &point, vector3d &center);
-		bool	allPointsInSphere(vector3d &center);
-		double	distMaxPointsInSphere(vector3d &center);
-		double	computeAngle(vector3d &p1, vector3d &p2, vector3d &p3, vector3d &axe);
-		int		getKey(int a, int b);
-		int		getKey(turnData& td);
-		double	getKeyByAngle(turnData& td);
-		void	travelCover(VVRSFace s);
-		void	printSphere(VVRSFace& s);
-		void	cover(void);
-		void	readVertex(const std::string& filename);
-		void	output(const std::string& rootPath);
-		bool	findFirstTriangle(unsigned &i,unsigned &j,unsigned &k,vector3d &c);
-	#pragma endregion
-	#pragma region "private static methods"
-	private:
-	#pragma endregion
-	#pragma region "protected data"
-	protected:
-	#pragma endregion
-	#pragma region "protected static data"
-	protected:
-	#pragma endregion
-	#pragma region "private data"
-	private:
-		double							_r;
-		double							_R;
-		std::vector<vector3d>			_points;
-		std::vector<VVRSFace>			_spheres;
-		std::set<int,std::less<int> >	_index;
-		double							_epsilon;
-		bool							_ccw;
 
-	#pragma endregion
-	#pragma region "private static data"
-	private:
-	#pragma endregion
-	#pragma region "friendship declarations"
-	#pragma endregion
 
-	};
+  class SmoothHullGeneratorVVR
+  {
+#pragma region "public structures"
+    public:
+#pragma endregion
+#pragma region "protected structures"
+    protected:
+#pragma endregion
+#pragma region "private structures"
+    private:
+      struct turnData
+      {
+        int			_p1;				//we turn around [_p1,_p2]
+        int			_p2;
+        int			_p3;				//we remember the third point of the previous sphere
+        vector3d		_previousCenter;	//center of the sphere we turn (/come) from
+      };
+#pragma endregion
+#pragma region "public data"
+    public:
+#pragma endregion
+#pragma region "constructors"
+    private:
+    protected:
+    public:
+      SmoothHullGeneratorVVR(double r, double R);
+      SmoothHullGeneratorVVR(std::vector<vector3d>& points, double r, double R);
+#pragma endregion
+#pragma region "accessors"
+    public:
+#pragma endregion
+#pragma region "public methods"
+    public:
+      //WARNING : be sure that there is no double points in the cloud
+      void	loadGeometry(const std::string& filename);
+      void	compute3DSMaxHull(const std::string& rootPath);
+      void	computeSmoothHull(std::vector<vector3d>& outPoints, std::vector<VVRSFace>& outSFaces);
+      void	computeVVR(const std::string& filename);
+      void	computeVVR_WithPolyhedron(const std::string& filename);
+      void	computeVVR_Prime(const std::string& filename);
+
+#pragma endregion
+#pragma region "public static methods"
+    public:
+#pragma endregion
+#pragma region "protected methods"
+    protected:
+#pragma endregion
+#pragma region "protected static methods"
+    private:
+#pragma endregion
+#pragma region "private methods"
+    private:
+      bool	findCenter(int p1, int p2, int p3, vector3d &center);
+      bool	isInSphere(vector3d &point, vector3d &center);
+      double	distInSphere(vector3d &point, vector3d &center);
+      bool	allPointsInSphere(vector3d &center);
+      double	distMaxPointsInSphere(vector3d &center);
+      double	computeAngle(vector3d &p1, vector3d &p2, vector3d &p3, vector3d &axe);
+      int		getKey(int a, int b);
+      int		getKey(turnData& td);
+      double	getKeyByAngle(turnData& td);
+      void	travelCover(VVRSFace s);
+      void	printSphere(VVRSFace& s);
+      void	cover(void);
+      void	readVertex(const std::string& filename);
+      void	output(const std::string& rootPath);
+      bool	findFirstTriangle(unsigned &i,unsigned &j,unsigned &k,vector3d &c);
+#pragma endregion
+#pragma region "private static methods"
+    private:
+#pragma endregion
+#pragma region "protected data"
+    protected:
+#pragma endregion
+#pragma region "protected static data"
+    protected:
+#pragma endregion
+#pragma region "private data"
+    private:
+      double							_r;
+      double							_R;
+      std::vector<vector3d>			_points;
+      std::vector<VVRSFace>			_spheres;
+      std::set<int,std::less<int> >	_index;
+      double							_epsilon;
+      bool							_ccw;
+
+#pragma endregion
+#pragma region "private static data"
+    private:
+#pragma endregion
+#pragma region "friendship declarations"
+#pragma endregion
+
+  };
 }
 
 #endif	//SCD_SMOOTH_HULL_GENERATOR_VVR_H
