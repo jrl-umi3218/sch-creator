7f95e862d3613ce8a065d710d8a7142bfa3f0dca
Francois Keith <keith@lirmm.fr>
2014-05-19 00:49:29 +0200

    Add unit tests and cleanup the creation process.
    Also correct the vrml parsing process.

--- script/vrml2cloud.py
+++ script/vrml2cloud.py
@@ -17,25 +17,38 @@ class Vrml(object):
   def _readPoint(self, l):
     if ']' in l:
       self._isPoint = False
-    else:
-      p = l.split(' ')
-      p[2] = p[2][0:-1]
-      self.points.append(p)
-  
+
+    # parse each of the point in the current line
+    p = l.split(',')
+    for pi in p:
+      pii = pi.split()
+      if len(pii) == 3:
+        self.points.append(pii)
+      elif len(pii) == 0:
+        pass
+      elif len(pii) > 3:
+        print "The number of points of one face exceed 3..."
+      elif ']' in pii:
+        self._isPoint = False
+      else:
+        print "pii ", pii
+        print "wut?"
+
+
 class Cloud(object):
   def __init__(self, points):
     self._points = points
 
   def save(self, file):
     file.write('%s\n' % len(self._points))
-    data = ['[%s,%s,%s],' % (p[0], p[1], p[2]) for p in self._points]
+    data = ['%s %s %s' % (p[0], p[1], p[2]) for p in self._points]
     file.write(''.join(data))
 
 if __name__ == '__main__':
   import sys
 
   if len(sys.argv) != 3:
-    print 'Usage : %s file.vrml file.txt' % sys.argv[0]
+    print 'Usage : %s file.wrl file.txt' % sys.argv[0]
     sys.exit(1)
 
   vrml = Vrml()
